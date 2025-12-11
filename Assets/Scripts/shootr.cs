3dd79a1651c9ca7ae4cb283464e0a16843846a37
Gael <ghernan5@trinity.edu>
2025-12-11 12:44:38 -0600

    adding more levels

--- Assets/Scripts/shootr.cs
+++ Assets/Scripts/shootr.cs
@@ -6,10 +6,15 @@ public class shootr : MonoBehaviour
     public GameObject bulletPrefab;
     public GameObject target;
     public GameObject spawnLocation;
+    private SpriteRenderer spriteRenderer;
+    public Sprite IdleSprite;
+    public Sprite ShootSprite;
 
     void Start()
     {
+        target = FindFirstObjectByType<playerScript>().gameObject;
         StartCoroutine(SpawnBullets());
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     IEnumerator SpawnBullets()
@@ -18,7 +23,15 @@ public class shootr : MonoBehaviour
         {
             Quaternion rot = Quaternion.LookRotation(Vector3.forward, target.transform.position - spawnLocation.transform.position);
             Instantiate(bulletPrefab, spawnLocation.transform.position, rot);
+            StartCoroutine(ChangeSprite());
             yield return new WaitForSeconds(1f);
         }
+    }
+
+    IEnumerator ChangeSprite()
+    {
+        spriteRenderer.sprite = ShootSprite;
+        yield return new WaitForSeconds(0.5f);
+        spriteRenderer.sprite = IdleSprite;
     }
 }
